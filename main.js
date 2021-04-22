71d4e27e2734ba55d47da650ae7015ed97a4361b
etiennezastko <etiennezastko@hotmail.fr>
2021-04-22 17:03:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+
+gsap.to(".box", {duration: 2, x: 300});
+gsap.to(".grey", {duration: 3, rotation: 360, scale: 0.5});
+gsap.to(".green", {duration: 3, rotation: 360, scale: 0.5});
+gsap.to(".orange", {duration: 5, rotation: 180, scale: 0.8});
